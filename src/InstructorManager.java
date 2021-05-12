b35f9e766fd3dd72754832e9a09b97e154148fd4
Osman Bal <osmanbal190@gmail.com>
2021-05-12 16:42:35 +0300

    Update InstructorManager.java

--- src/InstructorManager.java
+++ src/InstructorManager.java
@@ -1,25 +1,16 @@
 public class InstructorManager {
-    public void AddHomework(Logger logger){
-        System.out.println("Ödev eklendi.");
+
+
+    public void Update(Logger logger){
+        System.out.println("Eğitmen güncellendi.");
     }
 
-    public void UpdateHomework(Logger logger){
-        System.out.println("Ödev güncellendi.");
+    public void Delete(Logger logger){
+        System.out.println("Eğitmen silindi.");
     }
 
-    public void DeleteHomework(Logger logger){
-        System.out.println("Ödev silindi.");
+    public void Add(Logger logger){
+        System.out.println("Eğitmen eklendi.");
     }
 
-    public void AddCourse(Logger logger){
-        System.out.println("Kurs eklendi.");
-    }
-
-    public void UpdateCourse(Logger logger){
-        System.out.println("Kurs güncellendi.");
-    }
-
-    public void DeleteCourse(Logger logger){
-        System.out.println("Kurs silindi.");
-    }
 }
